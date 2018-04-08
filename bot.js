dea13eaed8cff215513cb5272b1207061dba3245
AccountableMenace <kasparas.miciunas@gmail.com>
2018-04-08 18:47:13 +0300

    .

--- bot.js
+++ bot.js
@@ -168,7 +168,7 @@ function playAudio(voiceChannel, source) {
             //Create a stream to your file and pipe it to the stream
             //Without {end: false}, it would close up the stream, so make sure to include that.  
             fs.createReadStream(source).pipe(stream, { end: false });
-            
+           
             
         }
     });
